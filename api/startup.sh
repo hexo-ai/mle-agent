3f5d5da19cc241e194d8ec6e2082333a56db0d84
Steven John <stevenjpx2@gmail.com>
2023-11-09 12:49:21 +0530

    fix: increase timeout

--- api/startup.sh
+++ api/startup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn mle_agent.main:app -w 1 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 -t 1000
+gunicorn mle_agent.main:app -w 1 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 -t 1000000
