cfb6b84d6abfd8b657b25afe47f08842a1513535
Steven John <stevenjpx2@gmail.com>
2023-11-09 13:05:13 +0530

    fix(build): use uvicorn

--- api/startup.sh
+++ api/startup.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-gunicorn mle_agent.main:app -w 1 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 -t 1000000
+# gunicorn mle_agent.main:app -w 1 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 -t 1000000
+uvicorn mle_agent.main:app --host 0.0.0.0 --port 8000
